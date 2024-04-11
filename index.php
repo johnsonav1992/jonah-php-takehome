d19c8d98c6dc6d0fc62c5ca662489c9948f60f18
Alex Johnson <johnsonav1992@gmail.com>
2024-04-11 14:56:30 -0500

    set up css

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
-
-    echo "hello!!!";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div class="box">
+            <?php echo "Hi" ?>
+        </div>
+    </div>
+</body>
+</html>
