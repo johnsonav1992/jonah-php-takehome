2bbc7d469d2a942328118c7e0c50a0ca5b29a30a
Alex Johnson <johnsonav1992@gmail.com>
2024-04-12 10:48:42 -0500

    starting cards

--- components/themes-section/themes_section.php
+++ components/themes-section/themes_section.php
@@ -1,3 +1,13 @@
-<div>
-    Themes
+<div class="theme-section-wrapper">
+    <h3>Demo Our Premium Themes</h3>
+    <div class="row-container gap-2">
+        <div class="video-card">
+            <div class="card-info-container">
+                <p class="card-title">Convert Prospects Faster With SkyLease</p>
+                <p class="card-details">
+                    Beautiful and effective: get the best of both worlds with SkyLease
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
