c04803677269649f8a849c9cb3c7b3c82deadcef
Alex Johnson <johnsonav1992@gmail.com>
2024-04-12 10:11:53 -0500

    footer basically done for now

--- components/footer/footer.php
+++ components/footer/footer.php
@@ -2,6 +2,7 @@
 $year = date("Y");
 
 ?>
+
 <footer class="footer-wrapper">
     <div class="footer-contact-section">
         <div class="row-container space-between full-width">
@@ -14,8 +15,8 @@ $year = date("Y");
         </div>
         <div class="contact-info gap-1">
             <div class="column-container gap-tiny">
-                <h6 class="white-text">866-272-5086</h6>
-                <p class="white-text body-2">hello@jonahdigital.com</p>
+                <h5 class="white-text">866-272-5086</h5>
+                <p class="white-text body-1">hello@jonahdigital.com</p>
             </div>
             <div class="row-container gap-tiny mt-tiny body-2">
                 <p class="white-text">Contact</p>
@@ -24,12 +25,24 @@ $year = date("Y");
             </div>
         </div>
     </div>
-    <div class="row-container gap-tiny white-text">
-        <p>© <?php echo $year ?> All Rights Reserved.</p>
-        <div class="row-container gap-tiny">
-            <p class="white-text">Privacy Policy</p>
-            <p class="white-text">|</p>
-            <p class="white-text">Site Map</p>
+    <div class="row-container space-between align-end">
+        <div class="row-container gap-tiny white-text">
+            <p>© <?php echo $year ?> All Rights Reserved.</p>
+            <div class="row-container gap-tiny">
+                <p class="white-text">Privacy Policy</p>
+                <p class="white-text">|</p>
+                <p class="white-text">Site Map</p>
+            </div>
+        </div>
+        <div class="order-online-card">
+            <div class="phone-container">
+                <img src="assets/phone.png" alt="phone" height="60px" class="phone">
+            </div>
+            <div class="column-container mt-tiny full-height">
+                <h6>Order Online!</h6>
+                <p class="body-2 mt-tiny">Visit orderjonah.com</p>
+            </div>
+            <img src="assets/arrow-right.svg" alt="arrow" width="24px" height="24px">
         </div>
     </div>
 </footer>
