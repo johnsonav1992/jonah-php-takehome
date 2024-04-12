2a04d7bff2e0fca4f6666b62013a38fb09566314
Alex Johnson <johnsonav1992@gmail.com>
2024-04-11 19:12:37 -0500

    connected to db and fetching data

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php 
+    ini_set('display_errors',1); 
+    error_reporting(E_ALL);
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'johnsonav1992');
+    define('DB_PASS', 'password');
+    define('DB_NAME', 'jonah_takehome');
+
+    $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    };
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php include 'config/db.php'; ?>
+
+<?php 
+    $sql = 'SELECT * from contact_info';
+    $items = mysqli_query($connection, $sql);
+?>
+
+<?php if(empty($items)): ?>
+    <p>There are no items</p>
+<?php endif; ?>
+
+<?php foreach($items as $item): ?>
+    <div>
+        <p><?php echo $item['first_name'] ?></p>
+        <p><?php echo $item['last_name'] ?></p>
+        <p><?php echo $item['email'] ?></p>
+        <p><?php echo $item['message'] ?></p>
+    </div>
+<?php endforeach; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +29,7 @@
 <body>
     <div>
         <div class="box">
-            <?php echo "Hi" ?>
+            <?php echo "Hiiii" ?>
         </div>
     </div>
 </body>
